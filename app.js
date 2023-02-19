d40a127d75914e93af31b4b4b37e60472ffe9500
worashf <uppert83@gmail.com>
2023-02-19 20:08:35 +0300

    Add auth route

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const  express =require("express")
+const cors  = require("cors")
+const auth  = require("./src/routes/authRoutes")
+const app  = express()
 
-app  = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
-app.use(express.json())
- app.use("/",(req,res)=>{
-    res.send({
-      success: true,
-      message:"Hello EasyTasker"
-    })
- })
+// authentication routes 
+app.use("/api/v1",auth )
 
 module.exports= app
