1670c9004eca31c1ddf19822b40695988963c828
worashf <uppert83@gmail.com>
2023-02-19 20:08:53 +0300

    Update entry

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const dotenv =  require("dotenv")
+const dotenv = require("dotenv")
 const app = require("./app")
 
+dotenv.config({ path: "src/configs/config.env" })
 
-dotenv.config({path:"src/configs/config.env"})
-
-
-
-
-const server = app.listen(process.env.SERVER_PORT, ()=>{
-console.log(`Server is started at ${process.env.SERVER_PORT}`);
+const server = app.listen(process.env.SERVER_PORT, () => {
+    console.log(
+        `Server started at port ${process.env.SERVER_PORT} in ${process.env.NODE_ENV}`
+      );
 })
