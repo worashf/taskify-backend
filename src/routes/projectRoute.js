adcfccf63df2be73c3599d595cc5f5dd2b4f154a
Worash <uppert83@gmail.com>
2023-04-19 17:51:43 +0300

    Add project routes

--- src/routes/projectRoute.js
+++ src/routes/projectRoute.js
@@ -1,8 +1,19 @@
-const express  = require("express")
-const {newProject} = require("../controllers/projectController")
-const router = express.Router()
+const express = require("express");
+const {
+  newProject,
+  updateProject,
+  updateProjectStatus,
+  deleteProject,
+  findUserProjects,
+} = require("../controllers/projectController");
+const router = express.Router();
 
+router
+  .route("/users/:userId/projects")
+  .post(newProject)
+  .put(updateProject)
+  .delete(deleteProject)
+  .get(findUserProjects);
+router.route("/users/:userId/projects/status").put(updateProjectStatus);
 
-router.route("/projects").post(newProject)
-
-module.exports  = router
+module.exports = router;
