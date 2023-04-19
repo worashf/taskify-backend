1a71cdc0f21977a328ec97b5330731ca66fe316a
Worash <uppert83@gmail.com>
2023-04-19 17:39:43 +0300

    Add  project controller methods

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -24,4 +24,49 @@ exports.newProject = async(req, res)=>{
         catch(err){
             throw  new Error(err)
         }
-        }
+}
+
+
+exports.updateProjectStatus = async (req, res) => {
+    try {
+        let {projectId, status}  = req.body
+        let project = await editProjectStatus(projectId, status) 
+        res.status(200).json({
+            success: true,
+            project
+       })   
+    } 
+    catch (err) {
+        throw new Error(err)
+    }
+}
+
+exports.deleteProject = async (req,res) => {
+    try {
+        let { projectId } = req.params
+        let result = await removeProject(projectId)
+        res.status(200).json({
+            success: true,
+            result
+       })  
+    }
+    catch (err) {
+        throw new Error(err)
+    }
+}
+exports.findUserProjects = async (req, res) => {
+    try {
+        let { userId }  = req.params
+        let projects = await getUserProjects(userId) 
+        res.status(200).json({
+            success: true,
+            projects
+       })  
+    }
+    catch (err) {
+         throw new Error(err)
+    }
+}
+        
+
+    
