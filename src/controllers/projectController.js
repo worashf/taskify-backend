5ebad1a5060073af6726229c7d3c8c7a938af3a2
Worash <uppert83@gmail.com>
2023-04-19 18:03:46 +0300

    Change async error handler

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,72 +1,50 @@
-const  {createProject,editProject,editProjectStatus, removeProject, getUserProjects} = require("../services/projectService")
+const {
+  createProject,
+  editProject,
+  editProjectStatus,
+  removeProject,
+  getUserProjects,
+} = require("../services/projectService");
+const catchAsyncError = require("../middlewares/catchAsyncError");
+exports.newProject = catchAsyncError(async (req, res) => {
+  let project = await createProject(req.body);
+  res.status(201).json({
+    success: true,
+    project,
+  });
+});
 
-exports.newProject = async(req, res)=>{
-    try{
-        let project  = await createProject(req.body)
-        res.status(201).json({
-            success:true, project
-        })
-    }
-    catch(err){
-        throw  new Error(err)
-    }
-    }
-  
-    exports.updateProject = async(req, res)=>{
-        try {
-            const {projectId, name, description}  = req.body
-            let project  = await editProject( projectId, name, description)
-            res.status(200).json({
-                success: true,
-                project
-            })
-        }
-        catch(err){
-            throw  new Error(err)
-        }
-}
+exports.updateProject = catchAsyncError(async (req, res) => {
+  const { projectId, name, description } = req.body;
+  let project = await editProject(projectId, name, description);
+  res.status(200).json({
+    success: true,
+    project,
+  });
+});
 
+exports.updateProjectStatus = catchAsyncError(async (req, res) => {
+  let { projectId, status } = req.body;
+  let project = await editProjectStatus(projectId, status);
+  res.status(200).json({
+    success: true,
+    project,
+  });
+});
 
-exports.updateProjectStatus = async (req, res) => {
-    try {
-        let {projectId, status}  = req.body
-        let project = await editProjectStatus(projectId, status) 
-        res.status(200).json({
-            success: true,
-            project
-       })   
-    } 
-    catch (err) {
-        throw new Error(err)
-    }
-}
-
-exports.deleteProject = async (req,res) => {
-    try {
-        let { projectId } = req.params
-        let result = await removeProject(projectId)
-        res.status(200).json({
-            success: true,
-            result
-       })  
-    }
-    catch (err) {
-        throw new Error(err)
-    }
-}
-exports.findUserProjects = async (req, res) => {
-    try {
-        let { userId }  = req.params
-        let projects = await getUserProjects(userId) 
-        res.status(200).json({
-            success: true,
-            projects
-       })  
-    }
-    catch (err) {
-         throw new Error(err)
-    }
-}
-        
-
-    
+exports.deleteProject = catchAsyncError(async (req, res) => {
+  let { projectId } = req.params;
+  let result = await removeProject(projectId);
+  res.status(200).json({
+    success: true,
+    result,
+  });
+});
+exports.findUserProjects = catchAsyncError(async (req, res) => {
+  let { userId } = req.params;
+  let projects = await getUserProjects(userId);
+  res.status(200).json({
+    success: true,
+    projects,
+  });
+});
