1c47ebc741b54f83b1d8fd5df20bcdbd6f35aad0
Worash <uppert83@gmail.com>
2023-04-19 18:09:56 +0300

    Add  Error handler

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -6,6 +6,7 @@ const {
   getUserProjects,
 } = require("../services/projectService");
 const catchAsyncError = require("../middlewares/catchAsyncError");
+const ErrorHandler  = require("../utils/errorHandler")
 exports.newProject = catchAsyncError(async (req, res) => {
   let project = await createProject(req.body);
   res.status(201).json({
@@ -14,35 +15,47 @@ exports.newProject = catchAsyncError(async (req, res) => {
   });
 });
 
-exports.updateProject = catchAsyncError(async (req, res) => {
+exports.updateProject = catchAsyncError(async (req, res, next) => {
   const { projectId, name, description } = req.body;
-  let project = await editProject(projectId, name, description);
+    let project = await editProject(projectId, name, description);
+    if (!project) {
+        return next(new ErrorHandler("Some thing went wrong project is not created.", 400))
+    }
   res.status(200).json({
     success: true,
     project,
   });
 });
 
-exports.updateProjectStatus = catchAsyncError(async (req, res) => {
+exports.updateProjectStatus = catchAsyncError(async (req, res, next) => {
   let { projectId, status } = req.body;
-  let project = await editProjectStatus(projectId, status);
+    let project = await editProjectStatus(projectId, status);
+    if (!project) {
+        return next(new ErrorHandler("Some thing went wrong project is  notupdated",400))
+    }
   res.status(200).json({
     success: true,
     project,
   });
 });
 
-exports.deleteProject = catchAsyncError(async (req, res) => {
+exports.deleteProject = catchAsyncError(async (req, res, next) => {
   let { projectId } = req.params;
-  let result = await removeProject(projectId);
+    let result = await removeProject(projectId);
+    if (!result) {
+        return next(new ErrorHandler("Some thing went wrong project is  not deleted.", 400))
+    }
   res.status(200).json({
     success: true,
     result,
   });
 });
-exports.findUserProjects = catchAsyncError(async (req, res) => {
+exports.findUserProjects = catchAsyncError(async (req, res, next) => {
   let { userId } = req.params;
-  let projects = await getUserProjects(userId);
+    let projects = await getUserProjects(userId);
+    if (!projects) {
+        return next(new ErrorHandler(`Some thing went wrong, could  not found  projectswith user  id:  ${userId}`,400))
+    }
   res.status(200).json({
     success: true,
     projects,
